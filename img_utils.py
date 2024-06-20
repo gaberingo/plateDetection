5ce2ce9fc2fa12723358c248f1bdce7e909c0108
gaberingo <iotkugabe@gmail.com>
2024-06-20 17:06:15 +0700

    [UPD] add preprocessing proccess

--- img_utils.py
+++ img_utils.py
@@ -1,29 +1,13 @@
-import string
 import easyocr
-import re
-
+import cv2
+import numpy as np
 # Initialize the OCR reader
 reader = easyocr.Reader(['id'], gpu=False)
 
 # Plate number format regex
-regex = r"^(?:([A-Z]{1,2})(\d{1,4}))([A-Z]{1,2})$"
+# regex = r"^(?:([A-Z]{1,2})(\d{1,4}))([A-Z]{1,2})$"
 
-# Mapping dictionaries for character conversion
-dict_char_to_int = {'O': '0',
-                    'I': '1',
-                    'J': '3',
-                    'A': '4',
-                    'G': '6',
-                    'S': '5'}
-
-dict_int_to_char = {'0': 'O',
-                    '1': 'I',
-                    '3': 'J',
-                    '4': 'A',
-                    '6': 'G',
-                    '5': 'S'}
-
-def get_truck_id(license_plate, truct_ids):
+def get_truck_id(license_plate, truck_ids):
     """Mengambil ID truk berdasarkan koordinat plat nomor
 
     Args:
@@ -36,55 +20,11 @@ def get_truck_id(license_plate, truct_ids):
     
     x1, y1, x2, y2, _, _ = license_plate
     
-    for truct_id in truct_ids:
-        xt1, yt1, xt2, yt2, truct_id = truct_id
+    for truck_id in truck_ids:
+        xt1, yt1, xt2, yt2, truck_id = truck_id
         if x1 > xt1 and y1 > yt1 and x2 < xt2 and y2 < yt2:
-            return xt1, yt1, xt2, yt2, truct_id
+            return xt1, yt1, xt2, yt2, truck_id
     return -1,-1,-1,-1,-1
-
-def license_complies_format(text):
-    """
-    Check if the license plate text complies with the required format.
-
-    Args:
-        text (str): License plate text.
-
-    Returns:
-        bool: True if the license plate complies with the format, False otherwise.
-    """
-    if re.search(regex, text):
-        return True
-    return False
-
-
-def format_license(text):
-    """
-    Format the license plate text by converting characters using the mapping dictionaries.
-
-    Args:
-        text (str): License plate text.
-
-    Returns:
-        str: Formatted license plate text.
-    """
-    license_plate_ = ''
-    mapping = {
-        0: dict_int_to_char, 
-        1: dict_int_to_char, 
-        4: dict_int_to_char, 
-        5: dict_int_to_char, 
-        6: dict_int_to_char,
-        2: dict_char_to_int, 
-        3: dict_char_to_int
-    }
-    
-    for j in [0, 1, 2, 3, 4, 5, 6]:
-        if text[j] in mapping[j].keys():
-            license_plate_ += mapping[j][text[j]]
-        else:
-            license_plate_ += text[j]
-
-    return license_plate_
 
 def read_plate_text(plate_crop):
     """Baca teks plat nomor
@@ -101,9 +41,6 @@ def read_plate_text(plate_crop):
         _, text, confidence = detection
         
         text = text.upper().replace(' ', '')
-
-        # if license_complies_format(text):
-            # return format_license(text), confidence
         return text, confidence
     
     return None, None
@@ -117,30 +54,86 @@ def write_csv(results, output_path):
         output_path (str): Path to the output CSV file.
     """
     with open(output_path, 'w') as f:
-        f.write('{},{},{},{},{},{},{}\n'.format('frame_nmr', 'truct_id', 'truct_bbox',
+        f.write('{},{},{},{},{},{},{}\n'.format('frame_nmr', 'truck_id', 'truck_bbox',
                                                 'license_plate_bbox', 'license_plate_bbox_score', 'license_number',
                                                 'license_number_score'))
 
         for frame_nmr in results.keys():
-            for truct_id in results[frame_nmr].keys():
-                print(results[frame_nmr][truct_id])
-                if 'truct' in results[frame_nmr][truct_id].keys() and \
-                   'license_plate' in results[frame_nmr][truct_id].keys() and \
-                   'text' in results[frame_nmr][truct_id]['license_plate'].keys():
+            for truck_id in results[frame_nmr].keys():
+                print(results[frame_nmr][truck_id])
+                if 'truck' in results[frame_nmr][truck_id].keys() and \
+                   'license_plate' in results[frame_nmr][truck_id].keys() and \
+                   'text' in results[frame_nmr][truck_id]['license_plate'].keys():
                     f.write('{},{},{},{},{},{},{}\n'.format(frame_nmr,
-                                                            truct_id,
+                                                            truck_id,
                                                             '[{} {} {} {}]'.format(
-                                                                results[frame_nmr][truct_id]['truct']['bbox'][0],
-                                                                results[frame_nmr][truct_id]['truct']['bbox'][1],
-                                                                results[frame_nmr][truct_id]['truct']['bbox'][2],
-                                                                results[frame_nmr][truct_id]['truct']['bbox'][3]),
+                                                                results[frame_nmr][truck_id]['truck']['bbox'][0],
+                                                                results[frame_nmr][truck_id]['truck']['bbox'][1],
+                                                                results[frame_nmr][truck_id]['truck']['bbox'][2],
+                                                                results[frame_nmr][truck_id]['truck']['bbox'][3]),
                                                             '[{} {} {} {}]'.format(
-                                                                results[frame_nmr][truct_id]['license_plate']['bbox'][0],
-                                                                results[frame_nmr][truct_id]['license_plate']['bbox'][1],
-                                                                results[frame_nmr][truct_id]['license_plate']['bbox'][2],
-                                                                results[frame_nmr][truct_id]['license_plate']['bbox'][3]),
-                                                            results[frame_nmr][truct_id]['license_plate']['confidence'],
-                                                            results[frame_nmr][truct_id]['license_plate']['text'],
-                                                            results[frame_nmr][truct_id]['license_plate']['text_confidence'])
+                                                                results[frame_nmr][truck_id]['license_plate']['bbox'][0],
+                                                                results[frame_nmr][truck_id]['license_plate']['bbox'][1],
+                                                                results[frame_nmr][truck_id]['license_plate']['bbox'][2],
+                                                                results[frame_nmr][truck_id]['license_plate']['bbox'][3]),
+                                                            results[frame_nmr][truck_id]['license_plate']['confidence'],
+                                                            results[frame_nmr][truck_id]['license_plate']['text'],
+                                                            results[frame_nmr][truck_id]['license_plate']['text_confidence'])
                             )
         f.close()
+        
+# Noise Reduction
+def noise_removal(image):
+    kernel = np.ones((1, 1), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    kernel = np.ones((1, 1), np.uint8)
+    image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+    image = cv2.medianBlur(image, 3)
+    return (image)
+
+# Dilation and Erosion
+def thin_font(image):
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2,2),np.uint8)
+    image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.bitwise_not(image)
+    return (image)
+
+def thick_font(image):
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2,2),np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    image = cv2.bitwise_not(image)
+    return (image)
+
+# Sharpening image
+def sharpening_font(image):
+    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+    sharpened = cv2.filter2D(image, -1, kernel)
+    return sharpened
+
+def preprocess_image(image):
+    # Step 0: Resize image
+    image = cv2.resize(image, (image.shape[1]*3, image.shape[0]*3))
+    
+    # Step 1: Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Step 2: Apply Gaussian blur
+    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+    
+    # Step 3: Edge detection using Canny
+    edges = cv2.Canny(blurred, 50, 150)
+    
+    # Step 4: Morphological operations (dilation and erosion)
+    # kernel = np.ones((3, 3), np.uint8)
+    # closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+    
+    # Step 5: Adaptive thresholding
+    thresh = cv2.adaptiveThreshold(edges, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    
+    # Step 6: Find contours
+    # contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    return thresh
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ frm_num = -1
 
 result = {}
 
-while ret and frm_num < 250:
+while ret:
     rep, frame = cap.read()
     frm_num += 1
     result[frm_num] = {}
@@ -28,7 +28,7 @@ while ret and frm_num < 250:
             if names[int(cls)] == 'truck':
                 trucks.append([x1,y1,x2,y2,conf])
                 
-        truct_ids = mot_tracker.update(np.array(trucks))
+        truck_ids = mot_tracker.update(np.array(trucks))
         
         # Deteksi Plat Nomor
         for plate in results.boxes.data.tolist():
@@ -36,23 +36,33 @@ while ret and frm_num < 250:
             if names[int(cls)] == 'plate':
                 
                 # Mengambil ID truk berdasarkan koordinat plat nomor
-                xt1, yt1, xt2, yt2, truct_id = img_utils.get_truck_id(plate, truct_ids)
+                xt1, yt1, xt2, yt2, truck_id = img_utils.get_truck_id(plate, truck_ids)
                 
-                if truct_id != -1:
+                if truck_id != -1:
                     # Crop plat nomor
                     crop_plate = frame[int(y1):int(y2), int(x1):int(x2), :]
                     
-                    # Image processing plat nomor
-                    crop_plate_gray = cv2.cvtColor(crop_plate, cv2.COLOR_BGR2GRAY)
-                    crop_plate_thresh = cv2.threshold(crop_plate_gray, 75, 255, cv2.THRESH_BINARY_INV)[1]
+                    # Image processing
+                    crop_plate_thresh = img_utils.preprocess_image(crop_plate)
+                    cv2.imshow('thresh', crop_plate_thresh)
+                    # grayscale_crop_plate = cv2.resize(crop_plate, (crop_plate.shape[1]*3, crop_plate.shape[0]*3))
+                    # grayscale_crop_plate = img_utils.sharpening_font(grayscale_crop_plate)
+                    # grayscale_crop_plate = cv2.cvtColor(grayscale_crop_plate, cv2.COLOR_BGR2GRAY)
+                    
+                    
+                    # cv2.imshow('grayscale', grayscale_crop_plate)
+                    # crop_plate_thresh = cv2.adaptiveThreshold(grayscale_crop_plate,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,21,2)
+                    # crop_plate_thresh = img_utils.noise_removal(crop_plate_thresh)
+                    # crop_plate_thresh = img_utils.thin_font(crop_plate_thresh)
+                    # cv2.imshow('thresh', crop_plate_thresh)
                     
                     # Baca teks plat nomor
                     plate_text, plate_text_conf = img_utils.read_plate_text(crop_plate_thresh)
                     
                     # Mencatat hasil
                     if plate_text is not None:
-                        result[frm_num][truct_id] = {
-                            'truct':{'bbox':[xt1, yt1, xt2, yt2]},
+                        result[frm_num][truck_id] = {
+                            'truck':{'bbox':[xt1, yt1, xt2, yt2]},
                             'license_plate':{
                                 'bbox':[x1,y1,x2,y2],
                                 'text':plate_text,
@@ -61,8 +71,10 @@ while ret and frm_num < 250:
                             }
     else:
         break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
     
 img_utils.write_csv(result, 'output.csv')
 
-# cap.release()
-# cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
